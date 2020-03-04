2f4a2923b02080f17ad3ea7f8be63ed8998cadf6
jonathandly <dalyj138@gmail.com>
2020-03-03 20:50:03 -0500

    test_eating_cookies passes the small tests, but fails on the large tests. I don't think I implemented a cache correctly and that's why the test is failing.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,22 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def eating_cookies(n, cache={}):
+
+  if n in cache.keys():
+    return cache[n]
+
+  if n == 0 or n == 1:
+    return 1
+  elif n == 2:
+    return 2
+  elif n == 3:
+    return 4
+  elif n > 3:
+    result = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    cache[n] = result 
+
+    return result
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
